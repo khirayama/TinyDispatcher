2f0652cf85f1f377fa13547dbe39f45b182e4f2d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-08-06 10:44:31 +0900

    typo: tab -> space

--- TinyDispatcher.js
+++ TinyDispatcher.js
@@ -1,7 +1,7 @@
 export default class TinyDispatcher {
   on(event, callback) {
     this._events = this._events || {};
-    this._events[event] = this._events[event]	|| [];
+    this._events[event] = this._events[event] || [];
     this._events[event].push(callback);
   }
   off(event, callback) {
